8741325e27273c10a25186ef69868ca661230a1a
Okrymus inc <OKRYMUS@Panupongs-MacBook-Pro.local>
2019-01-19 23:24:10 -0500

    initial app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import DashboardPage from "./components/pages/DashboardPage";
+import SignupPage from "./components/pages/SignupPage";
+import ConfirmationPage from "./components/pages/ConfirmationPage";
+import ForgotPasswordPage from "./components/pages/ForgotPasswordPage";
+import ResetPasswordPage from "./components/pages/ResetPasswordPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
+import TopNavigation from "./components/nevigation/TopNavigation";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = ({ location, isAuthenticated }) => (
+  <div className="ui container">
+    {isAuthenticated && <TopNavigation />}
+    <Route location={location} path="/" exact component={HomePage} />
+    <Route
+      location={location}
+      path="/confirmation/:token"
+      exact
+      component={ConfirmationPage}
+    />
+    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <GuestRoute
+      location={location}
+      path="/signup"
+      exact
+      component={SignupPage}
+    />
+    <GuestRoute
+      location={location}
+      path="/forgot_password"
+      exact
+      component={ForgotPasswordPage}
+    />
+    <GuestRoute
+      location={location}
+      path="/reset_password/:token"
+      exact
+      component={ResetPasswordPage}
+    />
+    <UserRoute
+      location={location}
+      path="/dashboard"
+      exact
+      component={DashboardPage}
+    />
+  </div>
+);
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired
+  }).isRequired,
+  isAuthenticated: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    isAuthenticated: !!state.user.email
+  };
 }
 
-export default App;
+export default connect(mapStateToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import decode from "jwt-decode";
+import { composeWithDevTools } from "redux-devtools-extension";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import rootReducer from "./rootReducer";
+import { userLoggedIn } from "./actions/auth";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+if (localStorage.okrymusJWT) {
+  const payload = decode(localStorage.okrymusJWT);
+  const user = {
+    token: localStorage.okrymusJWT,
+    email: payload.email,
+    confirmed: payload.confirmed
+  };
+  store.dispatch(userLoggedIn(user));
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <Route component={App} />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
